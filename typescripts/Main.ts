fc8b1991cb7cf8ab0a95a2140f42dd7e40be2eac
ERSUCC <isaacbrown5763@gmail.com>
2026-07-16 15:25:09 -0500

    Bugfix: Don't show completion popup when in comment

--- typescripts/Main.ts
+++ typescripts/Main.ts
@@ -813,8 +813,9 @@ window.autocomplete = (context: CompletionContext) => {
     const procMatch = line.match(`^\\s*(to|to-report)\\s+(${identRegex}\\s*\\[)?`);
     const modMatch = line.match(/^\s*(import|export)\s+\[?/);
     const declMatch = line.match(`^\\s*(${window.program.decls.join("|")})\\s*\\[`);
+    const semiMatch = line.match(/^.*;/);
 
-    if (procMatch || modMatch || declMatch) {
+    if (procMatch || modMatch || declMatch || (semiMatch && (semiMatch[0].match(/"/g)?.length ?? 0) % 2 == 0)) {
       return null;
     }
 
